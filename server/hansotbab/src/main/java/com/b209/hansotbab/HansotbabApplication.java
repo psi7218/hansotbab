f042432a7e6030120069cfe7809aa51868109201
Jeongyeon <jeongyeon.park216@gmail.com>
2024-03-07 09:57:46 +0900

    Initial Commit
    Former-commit-id: b8dabb40db2d1d40a3069dac2b93e698f9d3b4dd

---
+++ server/hansotbab/src/main/java/com/b209/hansotbab/HansotbabApplication.java
@@ -0,0 +1,13 @@
+package com.b209.hansotbab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansotbabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HansotbabApplication.class, args);
+    }
+
+}
