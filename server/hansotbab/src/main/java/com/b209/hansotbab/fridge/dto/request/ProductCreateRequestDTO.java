da5a56e93a105699159d2c8556345c795cd54493
hyun0-25 <lisa1072@skku.edu>
2024-03-12 12:52:43 +0900

    [Add] Fridge 패키지 Swagger Skeleton 코드 작성
    Former-commit-id: de9b1065124604644c9812e83d6c229deae934bf

---
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/dto/request/ProductCreateRequestDTO.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41ed78469e03fbff44c36343c68d9296403a9884579e42e2f98978458baeb92b
+size 1162
---
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/dto/request/ProductUpdateReqestDTO.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7e46978424ef4b2a0c10c231f43ec0b64beda27de83e85b21bb1d0f439d7b2ae
+size 1078
