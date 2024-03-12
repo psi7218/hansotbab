b86cda2c5bd548ed1e4968dc6845ae7fbbee073a
방소영 <bjo8244@naver.com>
2024-03-12 09:08:41 +0900

    [Add] WishlistResponseDTO 필드 추가

--- server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/dto/response/WishlistResponseDTO.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/dto/response/WishlistResponseDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fdf6eb8f0b8ca2f792576fa2e41d0787d42772f8755f247d1115dfdbe9338c8
-size 819
+oid sha256:5609cb876e043b039e27db4e5385c5709a41d8880a18ca77fb2083e1431512c4
+size 946
