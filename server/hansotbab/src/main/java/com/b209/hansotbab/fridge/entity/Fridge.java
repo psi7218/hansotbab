8a1d87a810c9a4d60472586d0b1c6b91789a606e
hyun0-25 <lisa1072@skku.edu>
2024-03-13 17:44:58 +0900

    [Feat] Fridge Entity 작성

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/Fridge.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/Fridge.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b061d0d70e41e5caf2410fa6be71cac9c30a75bb528c232fb3876477e3956b2c
-size 1126
+oid sha256:e1e35d21b5803a74eb63cc93832901be562db2e28434994fb25ae8aa658a2016
+size 1171
---
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/FridgeLike.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4ff9d01660aeb39dfe07b397fd147f562c466647f7ba185594e8b534878de079
+size 920
