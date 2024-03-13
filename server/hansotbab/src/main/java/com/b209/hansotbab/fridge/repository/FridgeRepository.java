529f5f48c4c7613f995567b5903136aa5ad280f6
hyun0-25 <lisa1072@skku.edu>
2024-03-13 17:47:50 +0900

    [Feat] Fridge Repository 작성
    Former-commit-id: f009749badec01ace3c95580b27e14300172080d

---
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/repository/FridgeRepository.java
@@ -0,0 +1,7 @@
+package com.b209.hansotbab.fridge.repository;
+
+import com.b209.hansotbab.fridge.entity.Fridge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FridgeRepository extends JpaRepository<Fridge, Long> {
+}
