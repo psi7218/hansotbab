e274fa26a76c012c14aaf82b2df74fb08d2fc9c4
방소영 <bjo8244@naver.com>
2024-03-14 14:12:08 +0900

    [Feat] 삭제여부기반 냉장고조회 구현

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/repository/FridgeRepository.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/repository/FridgeRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5badd324e1415d3fd316a81dd78caa61f7d3ed14c33969fb9812b1cfa60c338a
-size 294
+oid sha256:c3fbfc14b27f335a12db9c340806ec56df26075b40c9b6cc8ade3f8d587efa54
+size 390
