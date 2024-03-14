f588ff6a21de2b06544bc9f8073b62b1a9bcf1e4
방소영 <bjo8244@naver.com>
2024-03-14 11:02:52 +0900

    [Feat] Wishlist DTO 생성
    Former-commit-id: e67b544da078b0e581a3549c4706e461c58c4ebf

--- server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/dto/request/WishlistRequestDTO.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/dto/request/WishlistRequestDTO.java
@@ -1,17 +1,3 @@
-package com.b209.hansotbab.wishlist.dto.request;
-
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-public class WishlistRequestDTO {
-
-    private final Long userId;
-    private final String wishlistContent;
-
-    @Builder
-    public WishlistRequestDTO(Long userId, String wishlistContent){
-        this.userId = userId;
-        this.wishlistContent = wishlistContent;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6e9f51bbca8cd3ffa4e21bb2e8739dab17e491f4051e01da5e30cceee66cfcd5
+size 609
