9ec449d70aeeee1594dab9bc415fa5f03c0c273d
주동현 <wn196785@gmail.com>
2024-03-14 09:18:42 +0900

    [Add] main, profile 페이지 추가
    Former-commit-id: c7a0d6cde19b664893ae2a3fc759064dd2e4c33a

--- flutter/hansotbab/lib/main.dart
+++ flutter/hansotbab/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9e554907811600b34ce092ecd72b3a822d7b9bbaebe50cb25c2f384eb24b391
-size 4567
+oid sha256:6607034a6bf97f91b50563ec37aacc7a10e08009d48512a6106e386c12b7ef00
+size 6185
---
+++ flutter/hansotbab/lib/map.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:32bbb25958f3c5f8b1903092db96f8e295783739b7c990038f6d7496b86b5e54
+size 644
