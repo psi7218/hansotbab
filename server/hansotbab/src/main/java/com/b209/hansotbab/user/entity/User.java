dc43c09140b23d3506d37da54d72eff4192df3ac
방소영 <bjo8244@naver.com>
2024-03-15 08:55:13 +0900

    [Refector] entity 필드 수정
    1. isDelete 삭제
    
    
    Former-commit-id: 52324ea6fb9aebde41802c27f501ae27f31690c1

--- server/hansotbab/src/main/java/com/b209/hansotbab/user/entity/User.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edf2edd0176f4e2b8ffa62c556dc35d26209431fc94959a17fa05adcd9dc4932
-size 801
+oid sha256:688add657c3b12877c0c76d5af7c5001fe57eee2f1604cc2277584939b8bc73e
+size 749
