7b9e19e6ac4b2fb56163298c43a86dc5fe4ea257
방소영 <bjo8244@naver.com>
2024-03-14 14:12:08 +0900

    [Feat] 삭제여부기반 냉장고조회 구현
    Former-commit-id: ea49e5158a805b908e5315fce4ada1c238e21c2d

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/repository/FridgeRepository.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/repository/FridgeRepository.java
@@ -4,6 +4,9 @@ import com.b209.hansotbab.fridge.entity.Fridge;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FridgeRepository extends JpaRepository<Fridge, Long> {
+    Optional<Fridge> findByFridgeIdAndIsDeleteFalse(Long fridgeId);
 }
