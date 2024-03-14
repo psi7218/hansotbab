ba1134d4140515315053597a0e4731c3708df3dc
방소영 <bjo8244@naver.com>
2024-03-14 13:56:09 +0900

    [Test] User의 관리자이름 추가

--- server/hansotbab/src/test/java/com/b209/hansotbab/wishlist/repository/WishlistRepositoryTest.java
+++ server/hansotbab/src/test/java/com/b209/hansotbab/wishlist/repository/WishlistRepositoryTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25f19294e6fd960f98cd31d4f2cf0e7d0c5d29eac9f1ad990ef82dcbe722051f
-size 2553
+oid sha256:69fa5013a59b94de6874f8ec904be9476dacc234d1ff8e9b21179904ea1da21f
+size 2638
