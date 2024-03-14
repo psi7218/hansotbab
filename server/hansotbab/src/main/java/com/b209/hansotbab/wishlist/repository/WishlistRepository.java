d37c3daf0268344b22bc6e76207b8709e85b0fbb
방소영 <bjo8244@naver.com>
2024-03-14 14:46:37 +0900

    [Feat] 삭제여부기반 위시리스트 조회 구현
    Former-commit-id: e180dfbffb4793922bca1ca59ef9d2f0076b2931

--- server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/repository/WishlistRepository.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/repository/WishlistRepository.java
@@ -1,14 +1,3 @@
-package com.b209.hansotbab.wishlist.repository;
-
-import com.b209.hansotbab.fridge.entity.Fridge;
-import com.b209.hansotbab.wishlist.entity.Wishlist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
-
-    List<Wishlist> findAllByFridge(Fridge fridge);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d9819d0a5c835de64e994ef705e7a8b43762438581478d0fca759334d00b71f
+size 572
