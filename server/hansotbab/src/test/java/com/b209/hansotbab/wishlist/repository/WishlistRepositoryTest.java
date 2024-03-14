daef1e146392641a8a68435e561a2b43530761cb
방소영 <bjo8244@naver.com>
2024-03-14 14:14:31 +0900

    [Rename] 파일위치변경
    1. userRepository를 repository폴더로 이동
    
    
    Former-commit-id: 30c4d8cfa72c2fe4fbf8ee4a0bec5796d34f13c2

--- server/hansotbab/src/test/java/com/b209/hansotbab/wishlist/repository/WishlistRepositoryTest.java
+++ server/hansotbab/src/test/java/com/b209/hansotbab/wishlist/repository/WishlistRepositoryTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69fa5013a59b94de6874f8ec904be9476dacc234d1ff8e9b21179904ea1da21f
-size 2638
+oid sha256:ec32f176239d5d136bd7cba31b051b5675f484f28334fdf89432943d36b34574
+size 2649
