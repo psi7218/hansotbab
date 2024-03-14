ab81bbbd958f9b765617e1ca4e81c0dd3c545f0c
주동현 <wn196785@gmail.com>
2024-03-14 09:28:45 +0900

    [Add] main 수정
    Former-commit-id: 3e326aee6ad5ce49165ee7b8d2df828cb4f79bd7

--- flutter/hansotbab/lib/pages/mypage.dart
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bf2d945a7fead11834458515a2370273a69bb356f05776cb038608851c1b2b99
-size 3939
--- flutter/hansotbab/lib/profile.dart
+++ flutter/hansotbab/lib/profile.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c025b51b3d4f940ce648857ab3fa84c6e9926905a0dff88c74c6295ca0c1008
+oid sha256:92bd6cf6101217ec3aefd680e3c105acec53fa7ce7ec1d4b5fe5083f9bf90bd7
 size 4543
