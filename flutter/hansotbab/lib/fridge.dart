9932c6cfa1dc5ed75cd63866fa1ea59ff3211e0a
박성인 <psi7218@naver.com>
2024-03-15 14:01:52 +0900

    [Design] 각각의 그리드 뷰, 더블 필터링 리스트 추가
    Former-commit-id: 29727b8f6afa3ad42067ee0f52719c4572d9cc83

--- flutter/hansotbab/lib/fridge.dart
+++ flutter/hansotbab/lib/fridge.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd391a368f71ff2415c806335c3a94a446b13c4be3739cf4d3d2669d89e1cb8a
-size 3973
+oid sha256:916aab9e93f1e2a43710eefabcacdd46f7501447185170e18c32bc79914d7c99
+size 9154
