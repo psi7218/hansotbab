d963d9be992ca15d2f312a7fe633452d92f8f8e3
hyun0-25 <lisa1072@skku.edu>
2024-03-15 10:16:51 +0900

    [Feat] Fridge, Product Entity 작성완료
    Former-commit-id: 82a12f04362c38e9be69c77e921b388b182f05da

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/Product.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/Product.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6e8796bc3e5fd6c2848d5a9e90a04381c01b091fb2e39e125be4f077e282276
-size 2269
+oid sha256:313a96b90bb728ef944bf99bfcd312e4d926859e902d4fbe45e9473a7c37a902
+size 2930
