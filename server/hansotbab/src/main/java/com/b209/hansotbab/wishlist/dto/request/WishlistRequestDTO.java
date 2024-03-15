ae73ef90b558cfdc438b077710256b8af56de0fd
방소영 <bjo8244@naver.com>
2024-03-15 09:03:03 +0900

    [Add] 생성자 필드 추가
    1. 로그인 유저를 확인하기 위한 WishlistRequestDTO의 user 필드 추가
    2. create시 리플렉션을 위한 기본생성자 추가
    
    
    Former-commit-id: 5620a51bd0577cdcd97b788669f9bb5fb60e0196

--- server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/dto/request/WishlistRequestDTO.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/dto/request/WishlistRequestDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e9f51bbca8cd3ffa4e21bb2e8739dab17e491f4051e01da5e30cceee66cfcd5
-size 609
+oid sha256:e9406dfafbcc439b73276b8868c3211f08e91c11779ac10226d339c2544e44d4
+size 738
