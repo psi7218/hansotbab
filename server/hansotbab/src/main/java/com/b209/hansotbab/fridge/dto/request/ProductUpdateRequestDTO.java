f22cdce6793ed190fd0944e0c2b58af7dfa873aa
이지영 <abcd23589534@gmail.com>
2024-03-18 10:27:19 +0900

    [FIX] ProductUpdateRequestDTO 변수명, getProductMemo() 메소드명 수정
    Former-commit-id: cc06799b95d3b965e0bda584007a1279e7eae5b5

---
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/dto/request/ProductUpdateRequestDTO.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:52febcde6c53c8385cee96f7d79c41654a1621c2274bceb313fd852c999f62cc
+size 1090
--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/Product.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/Product.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:313a96b90bb728ef944bf99bfcd312e4d926859e902d4fbe45e9473a7c37a902
-size 2930
+oid sha256:71e4b78bf77e7d5b1d1423e89190a390d9e5f58ce96ea969bf8088a562b04fe5
+size 2923
