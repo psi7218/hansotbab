2e490cb02ec16f466089596c680782ffaa7edabc
박성인 <psi7218@naver.com>
2024-03-19 17:09:47 +0900

    [Add] 모달창 및 하든 플로팅 버튼 추가
    Former-commit-id: 45eb2e64c71c9f54d0daf922e57a74ced564bd17

--- flutter/hansotbab/lib/fridge.dart
+++ flutter/hansotbab/lib/fridge.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:224dfd57fa99fa91813fda9a79a1cf05b38aaf78dee26184f4d80d4133185e58
-size 9462
+oid sha256:2979b3f4a14dd23e420e9c755bcc79267bdfcb4b11c286f99ff490a663376832
+size 13806
