60a1ac6e10176ce2f5577e55e1156394dd3588f7
박성인 <psi7218@naver.com>
2024-03-19 14:35:24 +0900

    [Add] 냉장고 페이지 드롭다운 생성
    Former-commit-id: a283198c20a19e150ae03df45da867349f526fa3

--- flutter/hansotbab/lib/fridge.dart
+++ flutter/hansotbab/lib/fridge.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:916aab9e93f1e2a43710eefabcacdd46f7501447185170e18c32bc79914d7c99
-size 9154
+oid sha256:224dfd57fa99fa91813fda9a79a1cf05b38aaf78dee26184f4d80d4133185e58
+size 9462
