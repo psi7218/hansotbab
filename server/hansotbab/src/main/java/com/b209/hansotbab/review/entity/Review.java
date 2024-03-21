63f7c7541d453172f8f9e40b8c2ed4fc743e7f44
방소영 <bjo8244@naver.com>
2024-03-21 09:09:34 +0900

    [Feat] Review Entity Dirty Checking
    1. Content 변경 추가
    2. IsDelete 변경 추가
    
    
    Former-commit-id: ae1856eb746f5fdf8c13398dc6d51b90b30e1452

--- server/hansotbab/src/main/java/com/b209/hansotbab/review/entity/Review.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/review/entity/Review.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a4df7cde214fdd2ef61c13f98d6eb5aefcee673206d179382a4494ecc9ad9e1
-size 1450
+oid sha256:ac2a3e196ffcfc6fb0452c85898dfb6f87aae998532e9f7518135fac29ea3921
+size 1715
