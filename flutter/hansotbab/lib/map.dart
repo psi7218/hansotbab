60b5b345e36a00781ed1233225f100f7f4eca2d9
주동현 <wn196785@gmail.com>
2024-03-21 17:45:41 +0900

    [Add] 지도 마커 추가
    Former-commit-id: c820f1ea43a068d59a7d68451a9d288a79c3bd54

--- flutter/hansotbab/lib/map.dart
+++ flutter/hansotbab/lib/map.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ed641d9dc854a0bb1734e661ef4d3bf98e9464b341ca5fd3bad7fd2d9642753
-size 644
+oid sha256:222ea77d6b13e89be750871e4e64bbb515bc508d974e8c1d48c00980eef49ac7
+size 5284
--- flutter/hansotbab/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/hansotbab/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ebc54b4d21464b875d1289b3dd5e44cd7e68930dbd6e8c7f954e438eb6ad499
-size 147
+oid sha256:7d70f38f9f0f92694a8e5506771afafc92d36c21b9d18fb245f3d739d2b7ccc5
+size 481
