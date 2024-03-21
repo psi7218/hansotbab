4cd4c0dd5d8bfb1adc327856741a6403b7ee5605
방소영 <bjo8244@naver.com>
2024-03-21 13:18:47 +0900

    [Refactor] 기기의 토큰 가져오는 로직 추가
    Former-commit-id: 2df31f1a1649e99f1bc2fc8184490057075600d2

--- flutter/hansotbab/lib/main.dart
+++ flutter/hansotbab/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a8361b3b5a4a0ad1255e178cdaa89b4b4336dbc69f718743f0b812389941a4c
-size 9058
+oid sha256:8eb0dcfa9173d2911ee368cb7c895cb5cd9b469d9a9bf9fafed6bf62c5d4d50e
+size 9213
