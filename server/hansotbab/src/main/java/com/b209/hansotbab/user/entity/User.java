cb007f0897985affc717654e0d846af353d68856
방소영 <bjo8244@naver.com>
2024-03-21 17:14:10 +0900

    [Add] User에 FirebaseToken 컬럼 추가
    1. FCM으로 알림줄 때 Target User 중 FireBaseToken을 발급해 확인 후 알림
    
    
    Former-commit-id: fb184c57e1173e1306257c8acd9b94c544b643ee

--- server/hansotbab/src/main/java/com/b209/hansotbab/user/entity/User.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36e992f9ce9c561770eadcba85312f983a84afdba66e49076243ed815070dd41
-size 814
+oid sha256:a37c0a396719b5fed049b9e3c88fc04fcf08a110e574d77c0f200052e5f088a1
+size 849
