9695874f34aae2d1b8d9d9ef86eee82e4e28736d
박성인 <psi7218@naver.com>
2024-03-22 12:23:51 +0900

    [Add] 게시판페이지 포스트잇 추가
    Former-commit-id: 20200cdb3e7c262e9cc26d5ed9cbd2505d75b3a3

--- flutter/hansotbab/lib/main.dart
+++ flutter/hansotbab/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e75bd61f78bae3fc1fe157d5e1663d2ce957d2f6500a766796006fd3d98edcc
-size 7167
+oid sha256:51d9bd12a612bce60c84e4f30a4beac7324ff31cee63e74087c9d0473e1bb1ef
+size 7213
