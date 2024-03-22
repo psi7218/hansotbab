2d877063195c7e6a6a562c89be7a70f6d00daff5
주동현 <wn196785@gmail.com>
2024-03-22 17:48:59 +0900

    [Add] 프로필 수정버튼 기능 추가
    Former-commit-id: 3f0e898077dbae9f3ab4c8be6676a72644eda577

--- flutter/hansotbab/lib/fixprofile.dart
+++ flutter/hansotbab/lib/fixprofile.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cef0472225cda443ce2aea3639d91f36e8c1364adbe93f40189a6daf4ff176cc
-size 1472
+oid sha256:67927941ba4b96af66cd70fe5ac17fcd103a8f7ae15ff89dab028e7d25781287
+size 4570
