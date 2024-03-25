f4d861d4cb061a73a0559578902dc9395e4432f3
jystella17 <jystella21617@gmail.com>
2024-03-25 14:01:43 +0900

    [Feat] 로그인 기능 및 ATK & RTK 발급 기능 추가 (테스트 완료)
    Former-commit-id: 79be34485a57cc898484aa5a432fa20f01f19831

--- server/hansotbab/src/main/java/com/b209/hansotbab/user/dto/request/AdminReqestDTO.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/user/dto/request/AdminReqestDTO.java
@@ -1,21 +1,3 @@
-package com.b209.hansotbab.user.dto.request;
-
-import lombok.Builder;
-import lombok.Getter;
-
-import java.io.Serializable;
-
-@Getter
-public class AdminReqestDTO implements Serializable {
-
-    private final String userName;
-    private final String userPhone;
-    private final Long fridgeId;
-
-    @Builder
-    public AdminReqestDTO(String name, String phone, Long fridgeId) {
-        this.userName = name;
-        this.userPhone = phone;
-        this.fridgeId = fridgeId;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e8c4c3c0d2649bfc8f9119e9370eb7a7c21a12c1eff77e3786be7f3a49345cd
+size 429
--- server/hansotbab/src/main/java/com/b209/hansotbab/user/dto/request/NicknameDTO.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/user/dto/request/NicknameDTO.java
@@ -1,19 +1,3 @@
-package com.b209.hansotbab.user.dto.request;
-
-import lombok.Builder;
-import lombok.Getter;
-
-import java.io.Serializable;
-
-@Getter
-public class NicknameDTO implements Serializable {
-
-    private final Long userId;
-    private final String newNickname;
-
-    @Builder
-    public NicknameDTO(Long userId, String newNickname) {
-        this.userId = userId;
-        this.newNickname = newNickname;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:96faaa915c9a175a6daf4a2d110e8363b7ea12f1e14cccfc042e87fb09288836
+size 444
