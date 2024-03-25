c9c16d26562d9f31edd0eaa2008fee57d63f1762
주동현 <wn196785@gmail.com>
2024-03-25 20:11:23 +0900

    [Add] 음식 라우팅 설정
    Former-commit-id: fb2cbae4b5fbacb007265def7ab5a2fb5993e2a0

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/ProductStatus.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/ProductStatus.java
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e99bdb7c1398ce14068242e18d4be32f4125be476adb24fbee864419f5bb872e
-size 108
+package com.b209.hansotbab.fridge.entity;
+
+public enum ProductStatus {
+
+    SOLDOUT, ENOUGH, INSUFFICIENT
+}
