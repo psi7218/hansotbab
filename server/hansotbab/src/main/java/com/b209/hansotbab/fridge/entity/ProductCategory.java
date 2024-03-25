bcc253d36a1ac4e1175da425206afe4ef29521d3
hyun0-25 <lisa1072@skku.edu>
2024-03-25 17:58:24 +0900

    [Feat] 음식 info데이터 json파일변환 구현
    Former-commit-id: d54466714caee1323a93bb85a0ef1365a32709ac

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/ProductCategory.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/ProductCategory.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:622d941faed5eef3997021f714aba1ea4caeb73bc12950f0e95eeb6194bd452a
-size 165
+oid sha256:6161cafbe07d9b6e4e67b166723e4790eee1c7edb710419eac8a1391d94709f1
+size 167
--- server/hansotbab/src/main/java/com/b209/hansotbab/review/dto/request/ReviewLikeRequestDTO.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/review/dto/request/ReviewLikeRequestDTO.java
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:08963ef97754c960a3bfbeff79599d696130a1cdf40d815b4f148c3c34b6daa6
-size 397
+package com.b209.hansotbab.review.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class ReviewLikeRequestDTO {
+
+    private final Long userId;
+    private final Long fridgeId;
+    private final Long reviewId;
+
+    public ReviewLikeRequestDTO(Long userId, Long fridgeId, Long reviewId){
+        this.userId = userId;
+        this.fridgeId = fridgeId;
+        this.reviewId = reviewId;
+    }
+
+}
