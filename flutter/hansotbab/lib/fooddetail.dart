8217d18552f92e440feb7584b01d5eb7a6f5c65b
주동현 <wn196785@gmail.com>
2024-03-25 20:11:23 +0900

    [Add] 음식 라우팅 설정
    Former-commit-id: d5d225826510e8164ec46c34dca3cf7676df0f99

---
+++ flutter/hansotbab/lib/fooddetail.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d645fd783d48d50299cd57562aa497934537b8fdb4ff4cd5eb24c3810e20e75a
+size 2479
--- flutter/hansotbab/lib/fridge.dart
+++ flutter/hansotbab/lib/fridge.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b0131433be9e44bcab9b7c3494d5add403bb018189ff8012a6c4183b7ecd5e7
-size 20423
+oid sha256:ed2a06a30c9105048c91a037414ccf7d418a4000f561f0321b12b07fe138a625
+size 19838
