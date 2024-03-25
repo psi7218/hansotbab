6cbc0cd131d31bcb8e7161c06c3b67dfc390831d
박성인 <psi7218@naver.com>
2024-03-25 22:20:24 +0900

    [Design] UI 개선
    Former-commit-id: e9f0c1e7cca727813886608e8b08bdf425d6cb5b

--- flutter/hansotbab/lib/fridge.dart
+++ flutter/hansotbab/lib/fridge.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:624f94628e7cd7262044501d840f2ea8ca5fe678e56a315a1448badb8b6033ba
-size 21060
+oid sha256:9aef250ac0485b62f7dc129381a8d65bfd014ec52bd333a8c662cc6ebbb8afc8
+size 21822
