d657dffa9ec2519831c8dc3962505ef4f3570cd4
방소영 <bjo8244@naver.com>
2024-03-27 15:25:33 +0900

    [Add] 로그인 로직 추가
    Former-commit-id: 8f06bbe8ee46b955a60ed42635967a9f16bde5e4

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/ProductCondition.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/entity/ProductCondition.java
@@ -1,6 +1,3 @@
-package com.b209.hansotbab.fridge.entity;
-
-public enum ProductCondition {
-
-    AMBIENT, FRIDGE, FREEZE
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb2e11d36be4ef5e6dd54e487f137a1bba22f29e83d1002b9c538988f7ef766f
+size 105
