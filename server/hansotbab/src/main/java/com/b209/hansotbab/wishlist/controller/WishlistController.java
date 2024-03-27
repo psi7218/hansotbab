f68429fb7cf3a45cc6ce179307e33d0596270997
방소영 <bjo8244@naver.com>
2024-03-27 15:55:22 +0900

    [Fix] UserPrincipal로 파라미터 변경
    Former-commit-id: b319a03ad680949bd9e91da484e347a591e312c0

--- server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/controller/WishlistController.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/controller/WishlistController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e99829cab2c61dae8056c345dbb4648124d72660596095f3b5c7be186355528d
-size 4049
+oid sha256:fad54a9e6fd141eabcb0eefaf54c5cdc9bf80b68f4d95de608457a7d296c51ae
+size 4153
