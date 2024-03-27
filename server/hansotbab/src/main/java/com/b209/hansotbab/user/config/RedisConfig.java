008bbbd6d5895dfd54498f8c4b7604e6ccb36921
jeongyeon <jeongyeon.park216@gmail.com>
2024-03-27 15:12:44 +0900

    [Fix] RedisConfig Value 수정
    Former-commit-id: cf0bdfaea51092a19cb95af947cbb0432f07ab16

--- server/hansotbab/src/main/java/com/b209/hansotbab/user/config/RedisConfig.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/user/config/RedisConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a469eee257aa1481158d776c499690f0480b06c4eaa431ee934cfb9b90ba508
-size 2707
+oid sha256:f67e6e6d30652536fb5a298de21870b302638e2d8b581abeb3cd27a6567b1793
+size 2712
