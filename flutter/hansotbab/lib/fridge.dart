ec9c387f7493c88e7ae47af01eaa4b1e618af1ba
박성인 <psi7218@naver.com>
2024-03-27 22:33:04 +0900

    [Fix] write 컴포넌트  분리
    Former-commit-id: ed1a40042e8c547e82517d0240e924cf1b911acd

--- flutter/hansotbab/lib/fridge.dart
+++ flutter/hansotbab/lib/fridge.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:095e917796deea19c36554d062219df6d8fc95884297daae50210d496bd3c8d5
-size 24483
+oid sha256:2595f69aeb91f79bf30e790c4996d5fd3bb72dbe8db7307d0d15697d01a4c9f6
+size 14499
---
+++ flutter/hansotbab/lib/write.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c59d03c1c3649e5d77e53db1a9b4f3d5b126b20f1befb2938a1407276f235bc9
+size 2950
