5450ece333e13c3fd04ec8e60f5c2193b440cb99
hyun0-25 <lisa1072@skku.edu>
2024-03-27 10:09:48 +0900

    [Fix] import 오류 수정
    Former-commit-id: 35e87ef3ff345b75e82c8b86c41ad3c21cd12903

--- server/hansotbab/src/main/java/com/b209/hansotbab/food/controller/FoodController.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/food/controller/FoodController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b5028be905ef871cc361758cff3d7971bc7044eccff506d78fb5825fdbce3b6
-size 1046
+oid sha256:672b1698448b7bacd55dc6ea803fdb35289859518a2eec21713c389df13eae18
+size 826
