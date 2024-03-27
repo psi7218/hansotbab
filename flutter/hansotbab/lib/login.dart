4d05f071aef95e1222a158965fba2e14e072380d
박성인 <psi7218@naver.com>
2024-03-27 17:32:02 +0900

    [Add] 즐겨찾기한 냉장고 리스트 불러오기
    Former-commit-id: e46789006d03977422059cf54b36d8f8ace4d9d2

--- flutter/hansotbab/lib/login.dart
+++ flutter/hansotbab/lib/login.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffdee029da63ada60addb7c8163ddcea72d69385979ad74b54d1dbfe9e0201c8
-size 5414
+oid sha256:fa425d9f6ab746710104526c2dcbed03c3e9b25d79debbed47c111eb173615c3
+size 5567
