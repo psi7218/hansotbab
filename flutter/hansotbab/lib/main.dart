d85debbfd6db6263909729a47159257a1e6e7801
박성인 <psi7218@naver.com>
2024-03-27 22:33:04 +0900

    [Fix] write 컴포넌트  분리
    Former-commit-id: 446fdd813053491a9461f213abe43e1cf9770dee

--- flutter/hansotbab/lib/main.dart
+++ flutter/hansotbab/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db4d2d69767dde72fa3727aeebd3c1950dda601a57226efd325cdcaf6c828f84
-size 12079
+oid sha256:8415eb465f961723f0e1e4280e2b0eb42efe6cf42997b69980c959ae0d402e51
+size 12920
