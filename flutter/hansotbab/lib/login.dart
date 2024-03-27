72d4631d8b0fbcc71d1d70fef4ce4e3da822ffd0
박성인 <psi7218@naver.com>
2024-03-27 16:57:55 +0900

    [Fix] 로그인 로직 수정
    Former-commit-id: 3ad95c696503fc3362216a0436886021f3db7edb

--- flutter/hansotbab/lib/login.dart
+++ flutter/hansotbab/lib/login.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:309127cb637a1d9b045b8bb04de11c2054fe587c2d0432e4825141c6284d18a8
-size 5326
+oid sha256:ffdee029da63ada60addb7c8163ddcea72d69385979ad74b54d1dbfe9e0201c8
+size 5414
