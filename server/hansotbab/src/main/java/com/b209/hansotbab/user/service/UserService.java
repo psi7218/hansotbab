d420a2d6b909f5ec3cb37aa3e695e263efd7c89d
jeongyeon <jeongyeon.park216@gmail.com>
2024-03-28 10:36:12 +0900

    [Fix] 오류 수정
    Former-commit-id: d71db1367084e20d1fe11bc5d0272d83a1d48dcb

--- server/hansotbab/src/main/java/com/b209/hansotbab/user/service/UserService.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:182bc715e1a61be738dd174e5127fd4bd53a562582decdc77acc0901eab725f7
-size 6816
+oid sha256:8b12973504b206fd640374e172dd93f2fcb8bdabf22b3cd0535489cfd4ac1de5
+size 6854
