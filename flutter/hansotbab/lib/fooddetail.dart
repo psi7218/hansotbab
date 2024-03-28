16e749b6561fa08571aafb5220392d80a8825745
박성인 <psi7218@naver.com>
2024-03-28 16:05:48 +0900

    [Merge] 합치는중
    Former-commit-id: 4a0fb7dad708c237c8fe8b475a9dd9c58de238f4

--- flutter/hansotbab/lib/fooddetail.dart
+++ flutter/hansotbab/lib/fooddetail.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fd8c775d95122a742338099950f13780dc9dd2a8c88bf7869da7a6d9fbf4699
-size 4660
+oid sha256:7ed53c1b8198c4c7cf359ac7fe26a548bbfa2bfe53e34367bd3b509cec26b228
+size 4662
--- flutter/hansotbab/lib/fridge.dart
+++ flutter/hansotbab/lib/fridge.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c980e4de36206e09d508e15d54f08daba79a0448ce6014216283b9d4dc73ac6
-size 15325
+oid sha256:dc6af556ccb106af61ae0ac6e9970a81ae73d827df03fe59a64501dbc6f03f1a
+size 15297
