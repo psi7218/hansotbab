90746b1720644370d17ad30b4b51291801a8d4d3
jeongyeon <jeongyeon.park216@gmail.com>
2024-03-28 10:35:11 +0900

    [Fix] Access Token 유효기간 2시간으로 임의 변경
    Former-commit-id: b9555bf8e49597274a75d81d2aae85dfd5034cba

--- server/hansotbab/src/main/java/com/b209/hansotbab/user/jwt/TokenProvider.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/user/jwt/TokenProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:044d59e17f191c0d112ce5da93a8eef7dc33b5d317dd7434db05aae45215c7c6
-size 5341
+oid sha256:873a95478c55a657aa733098ef552f901523d2311a5c03f03e82554dcb1a8583
+size 5065
