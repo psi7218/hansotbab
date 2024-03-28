c8809b5617dc6e6d1fa56dd8d526344e6517b21f
jeongyeon <jeongyeon.park216@gmail.com>
2024-03-28 11:00:25 +0900

    [Fix] Access Token 관련 오류 해결
    Former-commit-id: fd1e388b834db290ede279a7893d0e574ce9f3d2

--- server/hansotbab/src/main/java/com/b209/hansotbab/user/entity/User.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa4693798dd64ce66217e76d1a86628203ab6361ebe9b0264233fee5d19af78b
+oid sha256:8937de19e64435e5a0ee990c7ad06af7e41e701abe41e2a87118e8d68a2e7c3e
 size 2213
