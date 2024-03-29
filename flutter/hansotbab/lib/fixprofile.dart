261f3b9393b0aa071b5ee34ea63c32a304e7a323
주동현 <wn196785@gmail.com>
2024-03-29 13:19:50 +0900

    [Add] 프로필 수정 2
    Former-commit-id: f7a950ab5cfc235cfe620ef9b8fbedd86c391376

--- flutter/hansotbab/lib/fixprofile.dart
+++ flutter/hansotbab/lib/fixprofile.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a57c150df35c095a842f889c1791758fdcfa6ea113f7f5b08233c5334f5c8c6
-size 11238
+oid sha256:7d2f7c22947e8c3edbfdd6593a60fc531f74d0a9070c526ca45de13aeed5304f
+size 12131
