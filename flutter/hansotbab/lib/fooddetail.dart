3700658c2e939b6f2a2b138cd034ed7f27e98d15
박성인 <psi7218@naver.com>
2024-03-29 17:23:24 +0900

    [Add] 음식 상세 페이지 UI 완성
    Former-commit-id: cdfa3e1b7a5abf9018868006763865a3c1a7f93d

--- flutter/hansotbab/lib/fooddetail.dart
+++ flutter/hansotbab/lib/fooddetail.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a9f27282465b96221363566a1ff2214321361338b2b251d32a6759de5319e29
-size 16875
+oid sha256:c104facbe6d1d9de2eb0016e0d1b7c1c06a69cdd52f7be253a503ae13bd995c9
+size 18773
