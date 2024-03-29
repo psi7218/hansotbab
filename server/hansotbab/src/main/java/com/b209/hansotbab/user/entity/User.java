02b6f7b3465f8617a72b0603f72f663870cab898
jeongyeon <jeongyeon.park216@gmail.com>
2024-03-29 12:56:34 +0900

    [Fix] Flutter 전용 Access Token 추가
    Former-commit-id: 78996c400b125a1b15b3237f0d66a917fbaa8fab

--- server/hansotbab/src/main/java/com/b209/hansotbab/user/entity/User.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8937de19e64435e5a0ee990c7ad06af7e41e701abe41e2a87118e8d68a2e7c3e
-size 2213
+oid sha256:223d71a49ad57e1539e3fe7067e27ad9ec655f89cda831d967c3ab13fe30172e
+size 2209
