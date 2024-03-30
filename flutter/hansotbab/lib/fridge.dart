5b75bb38b337012573509b35a7b4b0c15d198926
박성인 <psi7218@naver.com>
2024-03-30 16:34:51 +0900

    [Add] 음식 상세페이지 기능 구현
    Former-commit-id: f7c151a87e270750d0e1bceb0e47fc13514685ac

--- flutter/hansotbab/lib/fridge.dart
+++ flutter/hansotbab/lib/fridge.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c1b82a9c754f9da790c8a7b6b3aa4829551e58fa26fceed1b81684ffc5e1b3d
-size 17036
+oid sha256:00e251f7ac2a705b536e5e747ca301ce59f40561a75fe210c1813e418ab65ebd
+size 16916
