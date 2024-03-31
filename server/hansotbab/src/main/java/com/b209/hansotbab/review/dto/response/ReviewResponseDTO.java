9e460c4d5e62cb16e7d035929dfc080eb55504a6
방소영 <bjo8244@naver.com>
2024-03-31 21:12:40 +0900

    [Feat] 냉장고별 리뷰 등록 기능 구현
    1. controller 구현 (중복 uri 수정)
    2. Product가 null일 경우 null로 responseDTO에 생성
    3. service, serviceImpl 구현
    
    
    Former-commit-id: 2e0ee0202a86561c63525d687f797392eaea7823

--- server/hansotbab/src/main/java/com/b209/hansotbab/review/dto/response/ReviewResponseDTO.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/review/dto/response/ReviewResponseDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:306fecdb344549f71110cac7c1b447d816c0a70e864456b62835b48eabbe062a
-size 2179
+oid sha256:5ec448b7243045107a0c3e992dce4b5a3c2516f56b8adbecb84074bff2156a64
+size 2216
