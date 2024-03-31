c8ce4d34be7408e711ed0e285cbd833653252867
jystella17 <jystella21617@gmail.com>
2024-03-31 19:10:51 +0900

    [Feat] Webclient 추가 -> Fast API로 음식 등록  Post 요청
    Former-commit-id: 9b890c2cf03a5ecdaf585c75593f3d1bea0d600b

--- server/hansotbab/src/main/java/com/b209/hansotbab/user/config/SecurityConfig.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/user/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88dc46e98c04f6827cb66bc6c422472f19f5e13d3c40912003589fab8d538cf6
+oid sha256:62f608d6e607de2b8a8915995c9759eff011a1bdcec6bd57e04a2190d7518e4c
 size 4038
