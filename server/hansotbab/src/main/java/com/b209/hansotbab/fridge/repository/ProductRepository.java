63ea1eba0d713520fd9605f6595243c4b4057b1f
방소영 <bjo8244@naver.com>
2024-04-01 12:32:24 +0900

    [Refactor] user별 감사인사로 변경
    1. 원래 작성자별 감사인사 목록에서 음식을 기부한 사람에게 감사인사 작성한 list로 변경
    
    
    Former-commit-id: b4e9eb89a3d2cfb1618f89c2e9a86056ace4f19c

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/repository/ProductRepository.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/repository/ProductRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3b02507011f897017969af6ac26eca55bec9cbe6f5104999b66b4999f0bde76
-size 852
+oid sha256:f5d5491240ae6350918a57c7091996f14a54a6f3d2dfefd1a451298adab98a16
+size 941
--- server/hansotbab/src/main/java/com/b209/hansotbab/review/repository/ReviewRepository.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/review/repository/ReviewRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0485acb36b8846cb01ce200bfc173eb09a56d646bc1953f4ccaad9a7bedc26f
-size 538
+oid sha256:bbbc10bdc49df6d59a9174bb51bd402aa445edb3c2abb5a79a6931173f2b810d
+size 596
