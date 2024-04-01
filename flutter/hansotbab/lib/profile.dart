05008b54f15027a589d8defa95d0b446a8dccdf8
주동현 <wn196785@gmail.com>
2024-04-01 17:35:54 +0900

    [Add] 즐찾
    Former-commit-id: 3dd282ebd130cc444b8666e824aa4b875ee0a6e1

--- flutter/hansotbab/lib/profile.dart
+++ flutter/hansotbab/lib/profile.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:163d4fe362bf9bbd6a7be11e885a5a3f4173cb9a65e16f8bbf157af877661ee5
-size 9369
+oid sha256:3f4ca574be5f5cd365bb10c1ae6bbb471fe2f125eca23a2053220749edb37999
+size 9616
