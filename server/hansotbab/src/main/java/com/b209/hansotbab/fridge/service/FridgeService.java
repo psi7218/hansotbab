daed378678509d702bc6524ad48462b9290e84e1
hyun0-25 <lisa1072@skku.edu>
2024-04-01 23:19:52 +0900

    [Feat] 유저-냉장고 즐겨찾기 여부 API 구현
    Former-commit-id: 14c52f774cb8372b4dfd4b0bf080ca252e5dcf97

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/service/FridgeService.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/service/FridgeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cd4d2da205289b36acf3401f80bfeac4618be1df36bbe289a5875966a74be24
-size 2016
+oid sha256:551a0bd9eae2ee3ced3cc214b2f911e2112193f94870c5ee755c39892be34e90
+size 2553
