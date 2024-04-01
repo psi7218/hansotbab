d38bf62b37ce73b3d3e67c473d5468deb962576b
박성인 <psi7218@naver.com>
2024-04-01 13:22:20 +0900

    [Add] 게시판 글쓰기 api 연동
    Former-commit-id: accd95e6638d213e4c0067fd3604ac67899bf607

--- flutter/hansotbab/lib/write.dart
+++ flutter/hansotbab/lib/write.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:694adecd1e37e69bc1f6b8aa83de1c7f70de6423e30b98b848e48cc26149f3f7
-size 2817
+oid sha256:740cb4f22bd75265da15614b8331ae4cbd71a3cfe5c9f97399ecd8b62c27242b
+size 4251
