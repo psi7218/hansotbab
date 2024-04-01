a63cc98141b9e35eef947a0ea19e5a13dafb8735
박성인 <psi7218@naver.com>
2024-04-02 02:17:48 +0900

    [Add] 여러가지 기능 수정 및 추가
    Former-commit-id: 9687946189c93c671b1c78a8b9ff5333ae2ae097

--- flutter/hansotbab/lib/fooddetail.dart
+++ flutter/hansotbab/lib/fooddetail.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30fb4101ea94823d4faeff766db9a5d82ec66c4e7b8fbd6b5aa3e4103c0fe7ac
-size 22006
+oid sha256:049d6225ffcac313ab789e2e2687feeca38dfffe16aa20c64e963c32aa476c7a
+size 23993
