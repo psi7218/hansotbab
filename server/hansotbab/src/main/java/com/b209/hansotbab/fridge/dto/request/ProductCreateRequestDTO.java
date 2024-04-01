9c4d5e75f851e55b4acf34916e7026b3a0a18b5f
hyun0-25 <lisa1072@skku.edu>
2024-04-01 11:14:37 +0900

    [Feat] 음식 등록 및 수정 AWS S3 연동
    Former-commit-id: 205ccc3ee06e84e62bb01e4ae81d772f84f554d1

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/dto/request/ProductCreateRequestDTO.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/dto/request/ProductCreateRequestDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76ec1860989bab982d05ca1a1469163da09dbb58291b1646893f4c55c4fe8b41
-size 2030
+oid sha256:3afa6148c7f35f39ed89889a4ea5a4920073379da2f4a50b970195188fe65a8d
+size 1941
