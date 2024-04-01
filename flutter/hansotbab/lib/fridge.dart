aba3cf1c17e93b95e72753d57e635805ccc4908c
박성인 <psi7218@naver.com>
2024-04-02 02:17:48 +0900

    [Add] 여러가지 기능 수정 및 추가
    Former-commit-id: 7458f8e428e8524904f0ad59ac7c2c7968a48483

--- flutter/hansotbab/lib/fridge.dart
+++ flutter/hansotbab/lib/fridge.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce0d8fac61b5598cb21112ebc9b4f52f824b54de61ec08da7765d8453adfe12a
-size 22875
+oid sha256:d229e6668d09a8d1d631bba6775139da3d93ed5376fb97282b485f92c8cced98
+size 23261
--- flutter/hansotbab/lib/main.dart
+++ flutter/hansotbab/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c3086d01e4dc69ccfc315fe0ae6110fb314eb5f3d772377dd8490d302393ae7
-size 13019
+oid sha256:6a6204fe99760495690be6050da29f473c97f3d4f39511dd00c0f53c046f94b5
+size 11951
