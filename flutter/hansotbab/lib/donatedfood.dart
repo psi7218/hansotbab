c77971d22986a57d51aff13d8947afa4a48404e2
주동현 <wn196785@gmail.com>
2024-04-01 10:17:43 +0900

    [Add] 0401 임시저장
    Former-commit-id: 4a3198a0935881cf4ddd5a261af4042ad95a871f

--- flutter/hansotbab/lib/donatedfood.dart
+++ flutter/hansotbab/lib/donatedfood.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfd5744e71b5223eef3f4f20739e32984a2d2bb280f05aae8b0f12c8f040e739
-size 10565
+oid sha256:3031eb84517b10c6c3bf801e363d090c7b35ff93247230f224c8490c4590799e
+size 9473
