6177b6460932413f1cf9189ecf8f45d468896e99
방소영 <bjo8244@naver.com>
2024-04-01 12:48:27 +0900

    [Refactor] 유저별 감사인사 조회 반환값 변경
    1. list가 empty일 때 exception 던지지 말고 빈 리스트로 반환
    
    
    Former-commit-id: fe6ee57b14ac152a33deecd4f804a9f22a5a7bef

--- server/hansotbab/src/main/java/com/b209/hansotbab/review/service/ReviewServiceImpl.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/review/service/ReviewServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74836dfb49c0954086624165b351da2c0abcd926d697f74749897bb3ee683c27
-size 5911
+oid sha256:7c92fb5ff3293d2b443daaa81faa04e632a77221b45553ce828daf5300b06d71
+size 5747
