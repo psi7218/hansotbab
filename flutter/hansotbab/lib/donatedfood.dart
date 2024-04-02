5465bf2af9f44a334cd92020d996a292ee2fe6ce
주동현 <wn196785@gmail.com>
2024-04-03 00:34:11 +0900

    [Add] 메인페이지 권한 수정
    Former-commit-id: c658c722156908aa42b73ac3270811e3ca7cbb05

--- flutter/hansotbab/lib/donatedfood.dart
+++ flutter/hansotbab/lib/donatedfood.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbd3d0edb6f776a13ec83579554225db66fdbeb96fe708567179a18d1da00c7c
-size 7100
+oid sha256:0e240f68da51b30e1460fb720987c739ed0046fa27a23022909e1a63a8c3f0e6
+size 8688
--- flutter/hansotbab/lib/main.dart
+++ flutter/hansotbab/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e3e7ad661493357dff48b632dea3eb7f842cc39ea9c639339a634c036829e02
-size 6207
+oid sha256:2d43a7453ffd286c990bd6232c094dc099040bc0bf4e85f38418c8cf6d00ebcb
+size 6673
