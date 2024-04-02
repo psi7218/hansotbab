c8cf6900ececb2540024bb46e47728f7d3e87a25
방소영 <bjo8244@naver.com>
2024-04-02 10:26:39 +0900

    [Refactor] UserFcmToken의 PK 수정
    1. 회원가입시 동시에 요청을 보내기 위해 User 테이블의 unique인 email로 변경
    
    
    Former-commit-id: cdcc3040fbb4e91881aef8c2df93e05a586482dc

--- server/hansotbab/src/main/java/com/b209/hansotbab/user/entity/UserFcmToken.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/user/entity/UserFcmToken.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ac434074fed42ebaaad2ca3c71c0c3f26745ab2eecc05152eef5894e22f9ef0
-size 721
+oid sha256:e097fe824bee3d1f6596ccbd058a473c20dfd897653969f2fddb1ce8225d19e2
+size 735
