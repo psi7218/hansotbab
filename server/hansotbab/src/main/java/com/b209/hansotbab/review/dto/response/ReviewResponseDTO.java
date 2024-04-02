e48e1d7671983ed3616ff1a3dc9a59679b325354
방소영 <bjo8244@naver.com>
2024-04-02 21:33:10 +0900

    [Refactor] Review, Wishlist createdDate 반환
    Former-commit-id: 986e9aad5d1260c94758c0e87bb03f54e40b4f56

--- server/hansotbab/src/main/java/com/b209/hansotbab/review/dto/response/ReviewResponseDTO.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/review/dto/response/ReviewResponseDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ec448b7243045107a0c3e992dce4b5a3c2516f56b8adbecb84074bff2156a64
-size 2216
+oid sha256:c356215ecea50ca37a8de951c48a2ec5ce94f1846a7c20bc913da78a22e9a1d0
+size 2299
--- server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/dto/response/WishlistResponseDTO.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/wishlist/dto/response/WishlistResponseDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d36c57cf21f9b5a4304a0e1f1ba9ee6ab03cf0e07b783b238b51d86471aa873f
-size 2016
+oid sha256:2d259c29db223a1dc24b20c6f607e351b212dad70abcf563f039c661c2dd4957
+size 2107
