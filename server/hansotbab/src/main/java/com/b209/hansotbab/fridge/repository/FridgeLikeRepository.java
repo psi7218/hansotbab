7f911d8ec7789b73e104ada9305f5a263bfc07c4
방소영 <bjo8244@naver.com>
2024-04-02 14:18:24 +0900

    [Refactor] 냉장고에 해당하는 즐겨찾기로 변경
    1. 냉장고에 해당하는 리뷰에 대한 entity에서 Long firdgeId에서 Fridge fridge로 파라미터 변경
    
    
    Former-commit-id: 5bf2b1ab61a11abde12ae0be0930eae08bcc58da

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/repository/FridgeLikeRepository.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/repository/FridgeLikeRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b0d8295ffe82c4232603805c2b969aee137aee14c8d060b7ff6e2e0d14a997e
-size 518
+oid sha256:3f22ec621c7d9ebd24574856d8f90117b250887debde4e174cf1c3f8e1976797
+size 566
