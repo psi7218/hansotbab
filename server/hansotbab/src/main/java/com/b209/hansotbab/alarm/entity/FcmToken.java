3b757771391152e6adfd0393c62a1f8e721bc46c
방소영 <bjo8244@naver.com>
2024-04-02 11:16:17 +0900

    [Refactor] FcmToken entity 생성
    1. User 테이블과 fcm token관련 테이블 반정규화
    
    
    Former-commit-id: 610ec12f303781d4f74670b6e2083670e7f32265

---
+++ server/hansotbab/src/main/java/com/b209/hansotbab/alarm/entity/FcmToken.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:349608f72454cb026dff5ce6a0b9bf24df62b28914cf639ea567ecce5651b8c8
+size 543
--- server/hansotbab/src/main/java/com/b209/hansotbab/user/entity/UserFcmToken.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e097fe824bee3d1f6596ccbd058a473c20dfd897653969f2fddb1ce8225d19e2
-size 735
