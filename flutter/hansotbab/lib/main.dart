f00fa9adb81b6930d7a6bfdbac669210b51bbd7c
이지영 <abcd23589534@gmail.com>
2024-04-02 16:38:01 +0900

    [Add] splash screen 추가
    Former-commit-id: 7f5e74a69211e4a68e7a4e462d62e8334b644bd3

--- flutter/hansotbab/lib/main.dart
+++ flutter/hansotbab/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c3086d01e4dc69ccfc315fe0ae6110fb314eb5f3d772377dd8490d302393ae7
-size 13019
+oid sha256:42bbbf517b35c129a0aa48998c235acac7aca750d55b3d476e33878919580a95
+size 13112
