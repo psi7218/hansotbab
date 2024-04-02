7f22db9c3f455ad14c7fdc1938d034ff29a4c6f4
방소영 <bjo8244@naver.com>
2024-04-02 09:12:06 +0900

    [Feat] FCM 알림 전송 기능 구현
    Former-commit-id: 2ebc2eceb2e200a91b98e577ff3cfc4952e65fb8

---
+++ server/hansotbab/src/main/java/com/b209/hansotbab/alarm/controller/AlarmController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fdb41c3c4572d89d16f78db003cc4702a5d1b087b30f58fbfa8e4d4c86f0a526
+size 817
---
+++ server/hansotbab/src/main/java/com/b209/hansotbab/alarm/service/AlarmService.java
@@ -0,0 +1,8 @@
+package com.b209.hansotbab.alarm.service;
+
+import com.b209.hansotbab.alarm.dto.request.AlarmRequestDTO;
+
+public interface AlarmService {
+
+    String sendAlarmByToken(AlarmRequestDTO alarmRequestDTO);
+}
