c6516437371da34bcbd154852874761a94adaec3
hyun0-25 <lisa1072@skku.edu>
2024-04-03 01:23:57 +0900

    [Refactor] 폰트 및 아이콘 크기 조절
    Former-commit-id: 1b34cd7755dc5c16f98eec62d0781d1b9b1e2316

--- flutter/hansotbab/lib/fridge.dart
+++ flutter/hansotbab/lib/fridge.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:372b3ec005a83be41bfda6144c6a7fe4dda365afb0d03812ea0ef1f102a8b0a0
-size 37954
+oid sha256:f5319f4509e87b115e487afdfe52fd4566cc2c7f732de44a14008272ec1bef88
+size 37952
