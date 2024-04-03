032e6f7aad55aad2436d02eadea2cac64c5edece
jeongyeon <jeongyeon.park216@gmail.com>
2024-04-03 17:41:56 +0900

    [Fix] WebClient 코드 수정
    Former-commit-id: 0c9523cda4bed4fbc9abce1f828e708cc08cc79c

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/service/WebClientService.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/service/WebClientService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3c77ee2cd269ac0ebf3c27f7aeff6127661071a9fe94dccdca618924daaa843
-size 1437
+oid sha256:095390c1b1f4a129c137e2c3baf6df34140b9f85db780e177abcb41240d6fc72
+size 1739
