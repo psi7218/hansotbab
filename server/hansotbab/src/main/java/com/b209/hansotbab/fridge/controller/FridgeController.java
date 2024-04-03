c9f1a43b95e692991422e56cf3e8a71a35f7b8a9
jeongyeon <jeongyeon.park216@gmail.com>
2024-04-03 17:41:56 +0900

    [Fix] WebClient 코드 수정
    Former-commit-id: e7afc5febd38decd6667c1b41c2b69789bcbd83d

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/controller/FridgeController.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/controller/FridgeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a416e4eaefbc9e97658a32687fd9f7e87675425a8eefb0021041afa78a8ba16
-size 12413
+oid sha256:9d3b0ea9b7a784efe7e98a0fc7ed2b185059a4260b16568905f6bcb0f565f143
+size 12512
