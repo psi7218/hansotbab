e4341232ddeb5e33d9f1553a9d8db1909e771591
jeongyeon <jeongyeon.park216@gmail.com>
2024-04-03 17:45:51 +0900

    [Fix] FastAPI 요청 URL 수정
    Former-commit-id: 0a39d60c76e40984c0b85758115731bf02061eea

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/service/WebClientService.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/service/WebClientService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:095390c1b1f4a129c137e2c3baf6df34140b9f85db780e177abcb41240d6fc72
-size 1739
+oid sha256:11708c714ad01f478d0b59c8af4882a41e46ed5f2c56f736cb4b3ca52bef4323
+size 1745
