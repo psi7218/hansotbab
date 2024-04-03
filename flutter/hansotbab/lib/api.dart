de88074aaede3b2a5e7d38cbfe680cd546956cfc
방소영 <bjo8244@naver.com>
2024-04-03 13:03:16 +0900

    [Refactor] http 응답 코드 수정
    Former-commit-id: 942c1c8609a7a3b33b33a2eed15b8f8d927abee6

--- flutter/hansotbab/lib/api.dart
+++ flutter/hansotbab/lib/api.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fd6b219f1dcd618bec012894b4b52f924e51faf9e40969fa351049b04fdaa3f
+oid sha256:968f8fa8e0363da26cbcd7c72f3c6fe196a5b2dc2fd611b75f9841414b1359bb
 size 2877
