e0029c60641712f607f99b02634a6a8f47f0973c
jeongyeon <jeongyeon.park216@gmail.com>
2024-04-03 17:57:40 +0900

    [Fix] WebClient Buffer Size 문제 해결
    Former-commit-id: ca9502139d53cb9d1c4c7217a89642f0eb258e56

--- server/hansotbab/src/main/java/com/b209/hansotbab/fridge/service/WebClientService.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/fridge/service/WebClientService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11708c714ad01f478d0b59c8af4882a41e46ed5f2c56f736cb4b3ca52bef4323
-size 1745
+oid sha256:e8caf411c8a191781f1486943bf9da4616c4b0165a2fdd6d73658dff92d3c6ce
+size 1727
